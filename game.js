7f8c19a3f11d9137e58efe92d38f487194bc1457
bob.darvin <bob.darvin188@xmail.ru>
2024-09-01 09:15:32 +0300

    Рисование поля для игры в крестики-нолики

--- game.js
+++ game.js
@@ -6,11 +6,31 @@ const beginButton = document.getElementById("beginButton");
 
 function draw(e){
 	
-	ctx.fillRect(25, 25, 100, 100);
-	
 	ctx.beginPath();
 	ctx.moveTo(100, 0);
-	ctx.lineTo(300, 0);
+	ctx.lineTo(100, 300);
+	ctx.lineTo(101, 300);
+	ctx.lineTo(101, 0);
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.moveTo(200, 0);
+	ctx.lineTo(200, 300);
+	ctx.lineTo(201, 300);
+	ctx.lineTo(201, 0);
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.moveTo(0, 100);
+	ctx.lineTo(300, 100);
+	ctx.lineTo(300, 101);
+	ctx.lineTo(0, 101);
+	ctx.fill();
+	ctx.beginPath();
+	ctx.moveTo(0, 200);
+	ctx.lineTo(300, 200);
+	ctx.lineTo(300, 201);
+	ctx.lineTo(0, 201);
 	ctx.fill();
 }
 
