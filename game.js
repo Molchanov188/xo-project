9e595c257561c3466024da6449750e5025324077
bob.darvin <bob.darvin188@xmail.ru>
2024-09-01 08:48:05 +0300

    Рисование в canvas

--- game.js
+++ game.js
@@ -1,2 +1,17 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
+ctx.fillRect(25, 25, 100, 100);
+
+const beginButton = document.search.beginButton;
+
+function draw(){
+	
+	ctx.fillRect(25, 25, 100, 100);
+	
+	ctx.beginPath();
+	ctx.moveTo(100, 0);
+	ctx.lineTo(300, 0);
+	ctx.fill();
+}
+
+beginButton.addEventListener("click", draw);
